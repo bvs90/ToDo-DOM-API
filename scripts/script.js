07145cf1d95519dbaaa0ee618b12c56395d830c9
Ben Stone <benjamin.v.stone@gmail.com>
2014-05-14 12:48:21 -0700

    Created a working addItem function

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,20 @@
 var todoModule = (function(){
-  /* private data */
+  /* private data / methods */
 
 
   /* public data / methods */
   return {
-    button: document.querySelector('button'),
-    form: document.querySelector('input'),
+    button: document.querySelector('.todoModule button'),
+    form: document.querySelector('.todoModule input'),
+    list: document.querySelector('.todoModule .list'),
 
     addItem: function(){
-      console.log(this.form);
+      var itemText = document.createTextNode(this.form.value);
+      var item = document.createElement('li');
+      item.appendChild(itemText);
+      this.list.appendChild(item);
+      this.form.value = '';
     },
-
-
   };
 
 }());
