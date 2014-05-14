7fc1d9a7f7a67835639179af308d2f0fa642853b
Ben Stone <benjamin.v.stone@gmail.com>
2014-05-14 11:58:59 -0700

    Created toDoModule, tested event handler for button click and used bind to create correct context in addItem function

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
+var todoModule = (function(){
+  /* private data */
+
+
+  /* public data / methods */
+  return {
+    button: document.querySelector('button'),
+    form: document.querySelector('input'),
+
+    addItem: function(){
+      console.log(this.form);
+    },
+
+
+  };
+
+}());
+
+console.log(todoModule);
+
+todoModule.button.addEventListener('click', todoModule.addItem.bind(todoModule));
 
