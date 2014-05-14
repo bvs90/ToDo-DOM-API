9aa5e27cb05db4f96501268cc66d75198f93debf
Ben Stone <benjamin.v.stone@gmail.com>
2014-05-14 15:58:48 -0700

    Added checkItem function

--- scripts/script.js
+++ scripts/script.js
@@ -10,13 +10,21 @@ var todoModule = (function(){
     list: document.querySelector('.todoModule .list'),
     items: document.querySelector('.todoModule .item-count'),
 
+
     addItem: function(){
       if(this.form.value === ''){
         alert('You must enter some text!');
       }else {
         var itemText = document.createTextNode(this.form.value);
         var item = document.createElement('li');
+
+        var checkbox = document.createElement('input');
+        checkbox.addEventListener('click', this.checkItem, true);
+        checkbox.setAttribute('type','checkbox');
+
         item.appendChild(itemText);
+        item.appendChild(checkbox);
+
         this.list.appendChild(item);
         this.form.value = '';
         
@@ -30,7 +38,19 @@ var todoModule = (function(){
       while(this.list.firstChild){
         this.list.removeChild(this.list.firstChild);
       }
+      this.items.innerHTML = 0; 
+    },
+
+    checkItem: function(){
+      console.log(this.parentElement);
+      var item = this.parentElement;
+      if(this.checked === true){
+        item.style.textDecoration="line-through";
+      }else{
+        item.style.textDecoration="none";
+      }
     }
+
 
   };
 
